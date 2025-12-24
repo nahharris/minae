54b75aed0119514e2cd1795a48b4995ea15d2f2c
Hannah Harrisonn <hannah@harrisonn.dev>
2025-12-24 09:20:37 -0300

    feat(blocks): add stone slab block

--- pkg/blocks/blocks.go
+++ pkg/blocks/blocks.go
@@ -3,8 +3,21 @@ package blocks
 import "log"
 
 var (
-	Air   = Register(&Block{ID: "minae/air", Name: "Air", Color: 0x00000000})
-	Stone = Register(&Block{ID: "minae/stone", Name: "Stone", Color: 0x828282FF})
+	Air       = Register(&Block{ID: "minae/air", Name: "Air", Color: 0x00000000})
+	Stone     = Register(&Block{ID: "minae/stone", Name: "Stone", Color: 0x828282FF})
+	StoneSlab = Register(&Block{
+		ID: "minae/stone_slab",
+		Name: "Stone Slab",
+		Color: 0x828282FF,
+		ModelSpec: ModelSpec{
+			Type: "slab",
+			Textures: map[string]string{
+				"top":    "minae/stone",
+				"bottom": "minae/stone",
+				"side":   "minae/stone",
+			},
+		},
+	})
 	Dirt  = Register(&Block{ID: "minae/dirt", Name: "Dirt", Color: 0x7F513DFF})
 	Grass = Register(&Block{
 		ID:    "minae/grass",
